2babbe3ecd9b1b050e790394e11f4297a866b0fc
Daan Debie <d.debie@thenewmotion.com>
2014-08-03 21:38:35 +0200

    Fix os error

--- publishconf.py
+++ publishconf.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
+import os
 import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
